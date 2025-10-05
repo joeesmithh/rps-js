a8233cbc02ce944664b8ab446c05e175068b73e4
joeesmithh <josmth3030@gmail.com>
2025-10-05 17:57:39 -0400

    feat: implement round play and score accumulation

--- rps.js
+++ rps.js
@@ -14,8 +14,53 @@ function getComputerChoice() {
 }
 
 function getHumanChioce() {
-    return(prompt("Enter a choice of rock, paper, or scissors: "));
+    return (prompt("Enter a choice of rock, paper, or scissors: "));
 }
 
-console.log(`Computer choice: ${getComputerChoice()}`);
-console.log(`Human choice: ${getHumanChioce()}`);
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanChoice = "", computerChioce = "") {
+    let combined = (humanChoice.toLowerCase() + computerChioce.toLowerCase());
+    switch (combined) {
+        case "rockrock":
+            console.log("Stalemate! You both played Rock.")
+            break;
+        case "rockpaper":
+            console.log("You lose! Paper beats Rock.")
+            computerScore++;
+            break;
+        case "rockscissors":
+            console.log("You win! Rock beats Scissors.")
+            humanScore++
+            break;
+        case "paperrock":
+            console.log("You win! Paper beats Rock.")
+            humanScore++;
+            break;
+        case "paperpaper":
+            console.log("Stalemate! You both played Paper.")
+            break;
+        case "paperscissors":
+            console.log("You lose! Scissors beats Paper.")
+            computerScore++;
+            break;
+        case "scissorsrock":
+            console.log("You lose! Rock beats Scissors.")
+            computerScore++;
+            break;
+        case "scissorspaper":
+            console.log("You win! Scissors beats Paper.")
+            humanScore++;
+            break;
+        case "scissorsscissors":
+            console.log("Stalemate! Yout both played Scissors.")
+            break;
+        default:
+            break;
+    }
+}
+
+let humanSelection = getHumanChioce();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
